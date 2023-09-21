3c0a8f0d09c464803179153ab916dd4dc01a8793
bruno <bruno.cabral.silva2018@gmail.com>
2023-09-21 17:10:21 -0300

    Inclusão de algumas tabelas ao projeto

--- src/components/JobStatistics.jsx
+++ src/components/JobStatistics.jsx
@@ -1,4 +1,20 @@
 import React from 'react';
+import { Chart } from 'react-google-charts';
+
+
+export const data = [
+  ['Biblotecas', 'Número de Usuários'],
+  ['React', 151],
+  ['Vue', 161],
+  ['Angular', 101],
+  ['Bootstrap', 151],
+  ['Taiwind', 14],
+];
+
+const options = {
+  title: "Bibliotecas",
+  is3D: true,
+}
 
 export default function JobStatistics() {
   return(
@@ -14,6 +30,27 @@ export default function JobStatistics() {
         </div>
       </div>
       <div className="rounded-xl w-full h-48 border border-gray-300 flex flex-row">
+          <Chart
+            chartType="PieChart"
+            data={ data }
+            options={ options }
+            width={"50%"}
+            height={"100%"}
+          />
+          <Chart
+            chartType="ColumnChart"
+            data={ data }
+            options={ options }
+            width={"50%"}
+            height={"100%"}
+          />
+          <Chart
+            chartType="BarChart"
+            data={ data }
+            options={ options }
+            width={"50%"}
+            height={"100%"}
+          />
       </div>
     </div>
   );
